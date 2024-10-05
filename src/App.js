ed5d9ff40aed5673062159059478db0affc15d3d
ThuThao <thaoptqe170211@fpt.edu.vn>
2024-10-05 17:02:11 +0700

    tho

--- src/App.js
+++ src/App.js
@@ -1,25 +1,218 @@
 import logo from './logo.svg';
 import './App.css';
+import {Container, Row, Col, Card} from "react-bootstrap";
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Carousel from 'react-bootstrap/Carousel';
+import Modal from 'react-bootstrap/Modal';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
+//import ExampleCarouselImage from 'components/ExampleCarouselImage';
+import { useState } from 'react';
+import img1 from './images/pizza1.jpg';
+import img2 from './images/pizza2.jpg';
+import img3 from './images/pizza3.jpg';
+import menu1 from './images/menu1.jpg';
+import menu2 from './images/menu2.jpg';
+import menu3 from './images/menu3.jpg';
+import menu4 from './images/menu4.jpg';
+
+function MydModalWithGrid(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Modal {...props} aria-labelledby="contained-modal-title-vcenter">
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Using Grid in Modal
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body className="grid-example">
+        <Container>
+          <Row>
+  <Col xs={12} md={8}>
+    {/* Nội dung của card ở đây */}
+    Card Title 1 {/* Hoặc nội dung khác */}
+  </Col>
+  <Col xs={6} md={4}>
+    {/* Nội dung của card */}
+    <button class="btn btn-primary">+</button>
+    <button class="btn btn-danger">-</button>
+    
+  </Col>
+</Row>
+
+<Row>
+  <Col xs={12} md={8}>
+    {/* Nội dung của card */}
+    Card Title 2
+  </Col>
+  <Col xs={6} md={4}>
+    {/* Nội dung của card */}
+    <button class="btn btn-primary">+</button>
+    <button class="btn btn-danger">-</button>
+  </Col>
+</Row>
+        </Container>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={props.onHide}>Close</Button>
+      </Modal.Footer>
+    </Modal>
   );
 }
 
+function App() {
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex) => {
+    setIndex(selectedIndex);
+  };
+ 
+  const [modalShow, setModalShow] = useState(false);
+
+  
+  return (
+    <>
+      <Container>
+        <Row>
+        <Navbar expand="lg" className="bg-body-tertiary">
+      <Container fluid>
+        <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <Nav.Link href="#action1">Home</Nav.Link>
+            <Nav.Link href="#action2">Link</Nav.Link>
+            <NavDropdown title="Link" id="navbarScrollingDropdown">
+              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action4">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action5">
+                Something else here
+              </NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link href="#" disabled>
+              Link
+            </Nav.Link>
+          </Nav>
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success ms-2">Search</Button>
+          </Form>
+        </Navbar.Collapse>
+        <Row>
+        <>
+      <Button variant="primary" onClick={() => setModalShow(true)}>
+        Items
+      </Button>
+
+      <MydModalWithGrid show={modalShow} onHide={() => setModalShow(false)} />
+    </>
+        </Row>
+      </Container>
+    </Navbar>
+        </Row>
+        <Row>
+        <Carousel activeIndex={index} onSelect={handleSelect}>
+      <Carousel.Item>
+      <img src={img1} text="First slide" /> 
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+      <img src={img2} text="Second slide" />
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+      <img src={img3} text="Third slide" />
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+        </Row>
+        <Row>
+        <Row className='mt-5'>
+          <Col>
+          <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={menu1} />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+       <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+          </Col>
+          <Col>
+          <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={menu2} />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+          </Col>
+          <Col>
+          <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={menu3} />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+          </Col>
+          <Col>
+            <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={menu4} />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+          </Col>
+        </Row>
+        </Row>
+        <Row>
+        
+        </Row>
+      </Container>
+    </>
+  )
+}
 export default App;
